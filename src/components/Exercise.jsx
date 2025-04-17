8712225a2ddbf713c540be00116ac7284ece9a59
Abdullah Alnoime <abdullahalnoime@gmail.com>
2025-04-17 16:29:08 +0300

    update

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -17,8 +17,6 @@ import {
 
 // Import AceEditor component
 import AceEditor from "react-ace";
-import ace from 'ace-builds';
-import 'ace-builds/webpack-resolver';
 
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-github";
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        react(),
-    ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // If needed, add any path aliases here
+    },
+  },
+  optimizeDeps: {
+    include: ['react-ace'],
+    exclude: ['ace-builds']
+  },
+  build: {
+    rollupOptions: {
+      // External packages that should not be bundled
+      external: [],
+    },
+  },
 });
